11a688c9f6059e4eaa4b25e707df4521b9ecd612
Ali Kartal <ali_kartal91@hotmail.com>
2022-07-01 22:16:18 +0300

    changed

--- Homework-Week-2/src/components/Homepage.js
+++ Homework-Week-2/src/components/Homepage.js
@@ -7,10 +7,11 @@ function Homepage() {
     const [title, setTitle] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    // Axios Api Call
     useEffect(() => {
         axios("https://jsonplaceholder.typicode.com/todos")
             // .then(res => console.log(res.data))
-            .then(res => setTitle(res.data))
+            .then(res => setTitle(res.data.slice(0, 50)))
             .catch(e => console.log("error:", e))
         setLoading(false)
     }, []);
@@ -29,41 +30,63 @@ function Homepage() {
         }
     }
 
- const shortTitle = () => {
+    // Sort Method
+    const sortTitle = () => {
         axios("https://jsonplaceholder.typicode.com/todos")
-            // .then(res => console.log(res.data))
-            .then(res => setTitle(res.data.slice(0,50).reverse()))
+            // Birinci Method = reverse() / İkinci Method = sort()
+            // .then(res => setTitle(res.data.slice(0, 50).reverse()))
+            .then(res => setTitle(res.data.slice(0, 50).sort((a, b) => b.id - a.id)))
             .catch(e => console.log("error:", e))
         setLoading(false)
-}
+    }
 
-const editLoading = () => {
-    window.alert("sonra yapacağuz....")
-}
+    // Sort Reset Method
+    const sortReset = () => {
+        axios("https://jsonplaceholder.typicode.com/todos")
+            // Birinci Method = reverse() / İkinci Method = sort()
+            // .then(res => setTitle(res.data.slice(0,50).reverse())) 
+            .then(res => setTitle(res.data.sort((a, b) => a.id - b.id)))
+            .catch(e => console.log("error:", e))
+        setLoading(false)
+    }
 
+    // Edit Method
+    const editLoading = () => {
+        window.alert("sonra yapacağuz....")
+    }
 
+    const wholePage = () => {
+
+        axios("https://jsonplaceholder.typicode.com/todos")
+            .then(res => setTitle(res.data))
+            .catch(e => console.log("error:", e))
+        setLoading(false)
+
+        const buttonDOM = document.getElementById("noneButton")
+        buttonDOM.style.display = "none"
+    }
 
 
     return (
         <div className='container mt-3'>
             <div className='row'>
 
-                <table className="table">
+                <table className="table" id="tableId">
                     <thead>
                         <tr className='' id='sort'>
-                            <th scope="col" onClick={() => shortTitle()}>#</th>
-                            <th scope="col" onClick={() => shortTitle()}>Title</th>
-                            <th scope="col" onClick={() => shortTitle()}>Completed</th>
-                            <th scope="col" onClick={() => shortTitle()}>Actions</th>
+                            <th scope="col" onClick={() => sortTitle()}>#</th>
+                            <th scope="col" onClick={() => sortTitle()}>Title</th>
+                            <th scope="col" onClick={() => sortTitle()}>Completed</th>
+                            <th scope="col" onClick={() => sortReset()}>Actions</th>
                         </tr>
                     </thead>
                     {
-                        loading == true ? <h1>LOADING...</h1> :
-                            title.slice(0, 50).map((item, key) => (
+                        loading ? <h1>"LOADING..."</h1> :
+                            title.map((item, key) => (
                                 <tbody key={key} id="tbodyId">
                                     <tr>
                                         <th scope="row">{item.id}</th>
-                                        <td>{item.title.toUpperCase().slice(0,1)}{item.title.toLowerCase().slice(1)}</td>
+                                        <td>{item.title.toUpperCase().slice(0, 1)}{item.title.toLowerCase().slice(1)}</td>
                                         <td>{item.completed ? "COMPLETED" : "Not Completed"}</td>
                                         <td>
                                             <button type="button" className='btn btn-danger' onClick={() => deletePost(item.id)}>Remove</button>
@@ -75,6 +98,16 @@ const editLoading = () => {
                     }
                 </table>
 
+                {
+                    loading ? <h1>"LOADING..."</h1> :
+                        <button
+                            type='button'
+                            id="noneButton"
+                            className='btn btn-outline-success'
+                            onClick={() => wholePage()}
+                        >Click to load the whole page
+                        </button>
+                }
             </div>
         </div>
     )
